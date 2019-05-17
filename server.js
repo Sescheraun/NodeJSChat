8b1585599fd907014ccfa56bfa0aa05c9da593f5
Robert Collier <35576077+Sescheraun@users.noreply.github.com>
2019-05-17 13:33:12 -0500

    Adding socket.ion support

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var app = express();
+var http = requre('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -18,6 +19,6 @@ app.post('/messages', (req, res) => {
     res.sendStatus(200);
 })
 
-var server = app.listen(3000, () => {
+var server = http.listen(3000, () => {
     console.log('Server is listening on port ', server.address().port)
 })
