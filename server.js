a7496653a6b78c212b6e63c1e07f313ec4e3b8cf
Robert Collier <35576077+Sescheraun@users.noreply.github.com>
2019-05-19 13:13:46 -0500

    mongo DB connected and tested

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var mongoose = require('mongoose')
+ 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
+// TODO: learn to make this secret
+var dburl = 'mongodb+srv://user:user@cluster0-6fvf1.mongodb.net/test?retryWrites=true'
 
 var messages = [{name : "Bob", message : "Yo"},{name : "Not Bob", message : "Yoyo"}]
 
@@ -22,6 +26,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log("A new user connected");
+})
+
+mongoose.connect(dburl, (err) => {
+    console.log('Mongo DB connection', err)
 })
 
 var server = http.listen(3000, () => {
